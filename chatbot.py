cfefc71cff68465e6bdd425f2e92dede26d2847f
Pranesh Kishan <152611093+PraneshKishan@users.noreply.github.com>
2024-04-09 11:39:16 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('D:\\MLprojects\\intelligent_chatbot\\intents.json').read())
+intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
